0eec0c80ecdd069c1e02995a9dd54508361c47c0
Naman13Kumawat <naman.kumawat07@gmail.com>
2022-06-04 13:26:11 +0530

    Add JWT Authentication

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,33 @@
 import express from "express";
+import {
+  updateUser,
+  deleteUser,
+  getUser,
+  getUsers,
+} from "../controllers/user.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
+// router.get("/checkauthentication", verifyToken, (req, res, next) => {
+//   res.send("Hello user, you are logged in!");
+// });
 
+// router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+//   res.send("Hello user, you are logged in and you can delete your account!");
+// });
 
-export default router
+// router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+//   res.send("Hello Admin, you are logged in and you can delete any account!");
+// });
+
+// UPDATE
+router.put("/:id",verifyUser, updateUser);
+// DELETE
+router.delete("/:id",verifyUser, deleteUser);
+// GET
+router.get("/:id",verifyUser, getUser);
+// GET ALL
+router.get("/",verifyAdmin, getUsers);
+
+export default router;
